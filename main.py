dbaa468fe705382afb393167ae5be812d5b693cf
Arthur <arthur.pere@free.fr>
2016-12-09 11:57:01 +0100

    with linh files

--- main.py
+++ main.py
@@ -1,7 +1,60 @@
 
+class gene:
+	
+	def __init__(self, nom_gene, id_gene, position_deb, position_fin, longeur) :
+		self.nom_gene = nom_gene
+		self.id_gene = id_gene
+		self.position_deb = position_deb
+		self.position_fin = position_fin
+		self.longeur = longeur
+
+
+		self.transcript = 0
+
+	def __repr__(self) :
+		return "id = %s, nom = %s, longeur = %d, transcript = %d"%(self.id_gene, self.nom_gene, self.longeur, self.transcript)
+
+
 
 # lecture des fichiers
+file = open("/home/arthur/Documents/Projet/proj_biocomp/examples/chrom3genes.gff","r")
 
-fichier = open("chrom3genes.gff","r")
+lines = file.readlines()
 
-fi
+
+for line in lines:
+	
+	if line[0] == "#":
+		continue
+
+	l = line.split()
+
+	if l[2] == "region":
+
+		longeur = int(l[4]) - (int(l[3]) - 1)
+
+		nom = l[-1].split(";")[1].split("=")[1]
+		id_C = l[-1].split(";")[0].split("=")[1]
+
+		print longeur, nom, id_C
+
+	else :
+
+		longeur = int(l[4]) - (int(l[3]) - 1)
+
+		nom_gene = l[-1].split(";")[1].split("=")[1]
+		id_gene = l[-1].split(";")[0].split("=")[1]
+
+		sens = str(l[6])
+
+		if sens == "+":
+			g = gene(nom_gene, id_gene, l[3], l[4],longeur)
+			print g
+
+		else :
+			g = gene(nom_gene, id_gene, l[4], l[3],longeur)
+			print g
+		
+
+
+
--- petite_simu.py
+++ petite_simu.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
-
+from numpy import *
+from math import *
+from random import *
 
 ### Fichier à traiter ###
 fichier = open("chrom3genes.gff","r")
 info_gene = open("info_gene.txt","w")
-### Variables globales ###
-#NB_MISMATCH = 2
+#-----------------------------------------------------------------------------------------------------------------------------
+#            PARAMETRES
+#-----------------------------------------------------------------------------------------------------------------------------
+global sigma_t
+global m
+global epsilon
 
-### Variables ###
-#nb_sequence_non_alignee = 0
-#nb_sequence_alignee_ambigue = 0
-#sequence_aligne = {}
-#sequence_aligne_ambigue = []
+sigma_t=-0.4
+epsilon=0.01
+m=1
 
-#for mis in range(NB_MISMATCH +1) : sequence_aligne[mis] = 0
+
+
 chaine ="#"
 nbr_ligne = 0
 
@@ -22,7 +27,6 @@ for ligne in fichier:
 	if chaine not in ligne:
 		ligne = ligne.split("\t")
 		if ligne[2]=="gene":
-			print ligne[8]
 			nom_gene = str(ligne[8].split(";")[1].split("=")[1]).rstrip('\n')
 			position_start = int(ligne[3])
 			postion_stop = int(ligne[4])
@@ -36,3 +40,51 @@ for ligne in fichier:
 fichier.close()
 info_gene.close()
 
+class gene:
+	def __init__(self, longeur):
+		self.longeur=longeur
+	def p(self): #c'est pour tester
+		return(self.longeur)
+
+class chromosome:
+	def __init__(self, taux_basal,nb,longeur):
+		self.chrome=[gene(longeur=longeur[i]) for i in range(nb) ]
+		self.taux_basal = taux_basal ##un vecteur des taux de basal
+		self.nb = nb ##nombre de gene sur le chromosome
+	
+	def fonction_sigma(self,sigma): ## le f(sigma) dans l'equation de taux d'init
+		g_sigma = m/(1+exp(sigma-sigma_t)/epsilon)
+		prob_sigma = exp(g_sigma)
+		return(prob_sigma)
+
+	def etape_init(self,sigma):
+
+		self.taux_init = [] ##vecteur des taux pour chaque gene
+		self.prob_init = [] ##vecteur des prob pour chaque gene
+		self.sigma=sigma
+		self.indice = []    ## vecteur 0 non transcrit, 1 oui
+		self.tab=[] #tester print la longeur
+		for i,x in enumerate(self.chrome): ##i indice gene
+			taux_i = self.taux_basal[i]*self.fonction_sigma(self.sigma[i])
+			self.taux_init.append(taux_i)
+			self.tab.append(x.p()) #appelle la longeur
+		for i,x in enumerate(self.chrome):
+			prob_i = self.taux_init[i]/(sum(self.taux_init))
+			prob = randint(0,1)
+			if prob_i<prob:
+				indice_i=0
+			else: indice_i=1
+			self.prob_init.append(prob_i)
+			self.indice.append(indice_i)
+		return(self.prob_init,self.indice,self.tab)
+
+	def simulation(self,sigma):
+		self.sigma=sigma
+		print self.etape_init(self.sigma)
+
+
+taux_basal=[100,20,1000]
+sigma = [1,1,1]
+longeur=[76,46,50]
+chrom1= chromosome(taux_basal,3,longeur)
+chrom1.simulation(sigma)
